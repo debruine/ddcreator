2b693000d0dd3f9d4397c952ae3fc6060cd09a03
Lisa DeBruine <debruine@gmail.com>
2018-06-25 13:45:24 -0400

    Some refactoring and variable renaming.

--- app.R
+++ app.R
@@ -3,74 +3,86 @@ library(shiny)
 library(shinydashboard)
 library(DT)
 
+rawdata <- NULL
+
 ui <- dashboardPage(
-  dashboardHeader(title = "Data Codebook Convertor"),
+  dashboardHeader(title = "DDConvertor"),
+  ## Sidebar Menu ----
   dashboardSidebar(
     sidebarMenu(
-      fileInput("inFile", "Your Data File", 
-                multiple = FALSE, accept = NULL, width = NULL,
-                buttonLabel = "Browse...", placeholder = "No file selected"),
-      menuItem("Contents", tabName = "contents"),
-      menuItem("Variables", tabName = "variables"),
-      menuItem("Define Levels", tabName = "define_levels")
+      menuItem("Upload Data", tabName = "upload_tab"),
+      menuItem("Variables", tabName = "variables_tab"),
+      menuItem("Define Levels", tabName = "define_levels_tab")
     )
   ),
-  # Contents ----
+  ## Upload Data ----
   dashboardBody(
     tabItems(
-    tabItem(tabName = "contents",
+    tabItem(tabName = "upload_tab",
       fluidRow(
         box(
-          title = "Data contents",
-          DTOutput("contents")
+          title = "Upload Data",
+          width = 12,
+          p("This is a working demo and many functions do not work yet."),
+          fileInput("inFile", "CSV Data File", 
+                    multiple = FALSE, width = NULL,
+                    accept = c(
+                      "text/csv",
+                      "text/comma-separated-values,text/plain",
+                      ".csv"
+                    ), 
+                    buttonLabel = "Browse...", 
+                    placeholder = "No file selected"
+          ),
+          DTOutput("display_rawdata")
         )
       )
     ),
-    # Variables ----
-    tabItem(tabName = "variables",
+    ## Variables ----
+    tabItem(tabName = "variables_tab",
       fluidRow(
         box(
           title = "Variables",
-          DTOutput("vars")
+          width = 12,
+          DTOutput("vars_table")
         )
       )
     ),
-    # Define Levels ----
-    tabItem(tabName = "define_levels",
-            fluidRow(
-              box(
-                title = "Define Levels",
-                selectInput("level_col_select",
-                            label = "Columns",
-                            choices = c(),
-                            multiple = FALSE), # add support for multiple edit
-                DTOutput("level_col")
-              )
-            )
+    ## Define Levels ----
+    tabItem(tabName = "define_levels_tab",
+      fluidRow(
+        box(
+          title = "Define Levels",
+          width = 12,
+          # TODO:add support for multiple edit
+          selectInput("level_col_select",
+                      label = "Columns",
+                      choices = c(),
+                      multiple = FALSE), 
+          DTOutput("level_col_table")
+        )
+      )
     )
   ))
 )
 
-
-
-
 server <- function(input, output, session) { 
+  ## Load data ----
   dat <- reactive({
     inFile <- input$inFile
     if (is.null(inFile)) return(NULL)
     
-    d <- read.csv(inFile$datapath)
-    d
+    rawdata <<- read.csv(inFile$datapath)
   })
   
-  # output$contents ----
-  output$contents <- renderDataTable({
-    datatable(dat())
+  ## output$display_rawdata ----
+  output$display_rawdata <- renderDataTable({
+    dat()
+    datatable(rawdata)
   })
   
-  # output$vars ----
-  output$vars <- renderDataTable({
-    rawdata <- dat()
+  ## output$vars_table ----
+  output$vars_table <- renderDataTable({
     colnames <- names(rawdata)
     
     updateSelectInput(session, "level_col_select", 
@@ -103,21 +115,20 @@ server <- function(input, output, session) {
     datatable(vartable, editable = TRUE)
   })
   
-  # output$level_col ----
-  output$level_col <- renderDataTable({
+  ## output$level_col_table ----
+  output$level_col_table <- renderDataTable({
     theCol <- input$level_col_select
-    rawdata <- dat()
     
     unique_vals <- sort(unique(rawdata[,theCol]))
     unique_desc <- unique_vals
     # TODO: get unique_desc from attributes if they exist
     
-    level_col_table <- data.frame(
+    level_col_data <- data.frame(
       "values" = unique_vals,
       "description" = unique_desc
     )
     
-    datatable(level_col_table, editable = TRUE)
+    datatable(level_col_data, editable = TRUE)
     
     # TODO: save values on change to attributes
   })
