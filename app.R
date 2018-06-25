8db737fe0976588a5683dfd98354ebe27525e1e0
Lisa DeBruine <debruine@gmail.com>
2018-06-25 12:11:57 -0400

    Tabbed format

--- app.R
+++ app.R
@@ -15,7 +15,9 @@ ui <- dashboardPage(
       menuItem("Define Levels", tabName = "define_levels")
     )
   ),
+  # Contents ----
   dashboardBody(
+    tabItems(
     tabItem(tabName = "contents",
       fluidRow(
         box(
@@ -24,6 +26,7 @@ ui <- dashboardPage(
         )
       )
     ),
+    # Variables ----
     tabItem(tabName = "variables",
       fluidRow(
         box(
@@ -32,15 +35,24 @@ ui <- dashboardPage(
         )
       )
     ),
+    # Define Levels ----
     tabItem(tabName = "define_levels",
             fluidRow(
               box(
-                title = "Define Levels"
+                title = "Define Levels",
+                selectInput("level_col_select",
+                            label = "Columns",
+                            choices = c(),
+                            multiple = FALSE), # add support for multiple edit
+                DTOutput("level_col")
               )
             )
     )
-  )
+  ))
 )
+
+
+
 
 server <- function(input, output, session) { 
   dat <- reactive({
@@ -51,13 +63,19 @@ server <- function(input, output, session) {
     d
   })
   
+  # output$contents ----
   output$contents <- renderDataTable({
     datatable(dat())
   })
   
+  # output$vars ----
   output$vars <- renderDataTable({
     rawdata <- dat()
     colnames <- names(rawdata)
+    
+    updateSelectInput(session, "level_col_select", 
+                      label = NULL, choices = colnames,
+                      selected = NULL)
     
     types <- sapply(rawdata, class)
     
@@ -75,7 +93,7 @@ server <- function(input, output, session) {
     })
     
     vartable <- data.frame(
-      Column.Names = colnames,
+      Column.Name = colnames,
       Description = NA,
       Data.Type = types,
       Unique.Values = unique_val_n,
@@ -83,6 +101,25 @@ server <- function(input, output, session) {
     )
     
     datatable(vartable, editable = TRUE)
+  })
+  
+  # output$level_col ----
+  output$level_col <- renderDataTable({
+    theCol <- input$level_col_select
+    rawdata <- dat()
+    
+    unique_vals <- sort(unique(rawdata[,theCol]))
+    unique_desc <- unique_vals
+    # TODO: get unique_desc from attributes if they exist
+    
+    level_col_table <- data.frame(
+      "values" = unique_vals,
+      "description" = unique_desc
+    )
+    
+    datatable(level_col_table, editable = TRUE)
+    
+    # TODO: save values on change to attributes
   })
 }
 
