d27e2362db0e25107647924ee852de9760fe28d5
Lisa DeBruine <debruine@gmail.com>
2018-07-26 09:42:22 -0400

    Moved interfaces out to separate files

--- app.R
+++ app.R
@@ -3,132 +3,46 @@ library(shiny)
 library(shinydashboard)
 library(DT)
 
+## interface files
+source("project_interface.R")
+source("help_interface.R")
+source("upload_interface.R")
+source("variables_interface.R")
+source("labels_interface.R")
+source("output_interface.R")
+
+## Global Variables ----
 rawdata <- NULL
 var_data <- NULL 
 level_col_data <- NULL
 attribute_storage <- list()
 
+## UI ----
 ui <- dashboardPage(
   dashboardHeader(title = "DDConvertor"),
-  ## Sidebar Menu ----
   dashboardSidebar(
     sidebarMenu(
       menuItem("1. Project Info", tabName = "project_tab"),
       menuItem("2. Upload Data", tabName = "upload_tab"),
       menuItem("3. Variables", tabName = "variables_tab"),
-      menuItem("4. Category Labels", tabName = "define_levels_tab"),
+      menuItem("4. Category Labels", tabName = "labels_tab"),
       menuItem("5. Output", tabName = "output_tab"),
-      menuItem("Help", tabName = "format_tab")
+      menuItem("Help", tabName = "help_tab")
     )
   ),
-  ## Upload Data ----
   dashboardBody(
     tabItems(
-      ## Project Info ----
-      tabItem(tabName = "project_tab",
-              fluidRow(
-                box(
-                  title = "Project Info",
-                  width = 12,
-                  p(
-                    "This is a working demo and many functions do not work yet. You can upload csv or excel files to test the process. We do not save any information and delete your data from the temp directory regularly."
-                  ),
-                  textInput("project_name", "Project Name"),
-                  textInput("project_author", "Project Authors"),
-                  textAreaInput("project_description", "Project Description")
-                )
-              )
-      ),
-      tabItem(tabName = "format_tab",
-              fluidRow(
-                box(
-                  title = "Metadata format",
-                  width = 12,
-                  p(
-                    "If metadata is included, this should be specified as a nested JSON file containing a name, a description, an author, and a variable object. The nested variable object must minimally encode a list of objects, one per variable. Each object has the following keys:"
-                  ),
-                  tags$ol(
-                    tags$li("variable: name of variable"),
-                    tags$li("description: description of variable"),
-                    tags$li("[type]: “string”, “int”, “float”, “bool”"),
-                    tags$li("[min]: minimum valid value (not necessarily observed value)"),
-                    
-                    tags$li("[max]: maximum valid value (not necessarily observed value)"),
-                    tags$li("[levels]: valid levels of a categorical (string) variable"),
-                    tags$li("[na] (bool): whether or not missing values are allowed"),
-                    tags$li(
-                      "[na_values]: values to be treated as missing or invalid observations. Defaults to [“NA”, “N/A”, “n/a”, “NaN”, “nan”]"
-                    ),
-                    tags$li(
-                      "[synonyms]: A comma-separated list of common synonyms for the variable name. These are user-defined and not constrained by a controlled vocabulary."
-                    )
-                  )
-                )
-              )),
-      tabItem(tabName = "upload_tab",
-              fluidRow(
-                box(
-                  title = "Upload Data",
-                  width = 12,
-                  p("This is a working demo and many functions do not work yet."),
-                  fileInput("inFile", "CSV/XLS(X) Data File", 
-                            multiple = FALSE, width = NULL,
-                            accept = c(
-                              'text/csv',
-                              'text/comma-separated-values,text/plain',
-                              '.csv',
-                              '.xls',
-                              '.xlsx'
-                            ), 
-                            buttonLabel = "Browse...", 
-                            placeholder = "No file selected"
-                  ),
-                  checkboxInput("header", "Data file has a header", TRUE),
-                  DTOutput("rawdata_table")
-                )
-              )
-      ),
-      ## Variables ----
-      tabItem(tabName = "variables_tab",
-              fluidRow(
-                box(
-                  title = "Variables",
-                  width = 12,
-                  DTOutput("vars_table")
-                )
-              )
-      ),
-      ## Define Levels ----
-      tabItem(tabName = "define_levels_tab",
-              fluidRow(
-                box(
-                  title = "Define Levels",
-                  width = 12,
-                  # TODO:add support for multiple edit
-                  selectInput("level_col_select",
-                              label = "Columns",
-                              choices = c(),
-                              multiple = FALSE), 
-                  DTOutput("level_col_table")
-                )
-              )
-      ),
-      ## Output options ----
-      tabItem(tabName = "output_tab",
-              fluidRow(
-                box(
-                  title = "Output",
-                  width = 12,
-                  downloadButton("output_csv", "Download CSV"),
-                  downloadButton("output_attributes", "Download Attributes (test)"),
-                  downloadButton("output_rdata", "Download R data file (test)")
-                )
-              )
-      )
-    ) # end tabItems()
-  )
-) # end dashboardPage()
+      project_tab,
+      upload_tab,
+      variables_tab,
+      labels_tab,
+      output_tab,
+      help_tab
+    ) # end tabItems
+  ) # end dashboardBody
+) # end dashboardPage
 
+## server ----
 server <- function(input, output, session) { 
   ## Load data ----
   dat <- reactive({
@@ -141,6 +55,10 @@ server <- function(input, output, session) {
     } else if (file_extension %in% c("xls", "xlsx")) {
       rawdata <<- as.data.frame(readxl::read_excel(inFile$datapath, 
                                                    col_names = input$header))
+    } else if (file_extension %in% c("sav")) {
+      rawdata <<- haven::read_sav(inFile$datapath)
+    } else if (file_extension %in% c("sas")) {
+      rawdata <<- haven::read_sas(inFile$datapath)
     }
     
     #save file name as global variable for writing
@@ -152,7 +70,7 @@ server <- function(input, output, session) {
     
     for (theCol in column_names) {
       vals <- sort(unique(rawdata[,theCol]))
-      desc <- as.character(unique_vals)
+      desc <- as.character(vals)
       # TODO: get value + description labels from SPSS attributes if they exist
       
       attribute_storage[[theCol]] <<- data.frame(
@@ -208,7 +126,7 @@ server <- function(input, output, session) {
       
       # editable - required
       # TODO: get from SPSS description column
-      description = NA,
+      description = "",
       
       # editable - optional
       type = types,
@@ -238,15 +156,22 @@ server <- function(input, output, session) {
               ))
   })
   
+  vars_proxy = dataTableProxy('vars_table', session)
+  
+  ## copy_var_to_desc_button
+  observeEvent(input$copy_var_to_desc_button, {
+    var_data[, 'description'] <<- var_data[, 'variable']
+    DT::replaceData(vars_proxy, var_data, resetPaging = F)
+  })
+  
   ## proxy saving variable data ----
-  vars_proxy = dataTableProxy('vars_table')
   observeEvent(input$vars_table_cell_edit,  {
     info = input$vars_table_cell_edit
     str(info)
     i = info$row
     j = info$col
     v = info$value
-    var_data[i,j] <<- coerceValue(v, var_data[i,j])
+    var_data[i,j] <<- v #coerceValue(v, var_data[i,j])
     replaceData(vars_proxy, var_data, resetPaging = F)
   })
   
@@ -260,14 +185,14 @@ server <- function(input, output, session) {
   })
   
   ## proxy saving level column data ----
-  level_col_proxy = dataTableProxy('level_col_table')
+  level_col_proxy = dataTableProxy('level_col_table', session)
   observeEvent(input$level_col_table_cell_edit,  {
     info = input$level_col_table_cell_edit
     str(info)
     i = info$row
     j = info$col
     v = info$value
-    level_col_data[i,j] <<- coerceValue(v, level_col_data[i,j])
+    level_col_data[i,j] <<- v #coerceValue(v, level_col_data[i,j])
     replaceData(level_col_proxy, level_col_data, resetPaging = F)
     
     # save level column data to temp storage, eventually to attributes
@@ -309,12 +234,9 @@ server <- function(input, output, session) {
         attr(rawdata[,i], "labels") <- temp
       }
       
-      
-      
       save(rawdata, file=file)
     }
   )
-  
   
 } # end server()
 
